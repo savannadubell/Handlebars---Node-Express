fa32b1421e47b2ed7e2e1015c27d1b5b29242650
Savanna Dubell <savannadubell@savannas-mbp-2.uconnect.utah.edu>
2019-05-14 20:56:56 -0600

    five

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-insert into burgers_tbl(burger_name) values ('Cheese Burger');
-insert into burgers_tbl(burger_name) values ('Hamburger');
+insert into burgers_tbl(burger_name, devoured) values ('Cheese Burger', 1);
+insert into burgers_tbl(burger_name, devoured) values ('Hamburger', 0);
 insert into burgers_tbl(burger_name) values ('Bacon Cheese Burger');
 insert into burgers_tbl(burger_name) values ('Garlic Burger');
